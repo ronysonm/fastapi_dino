f97b3f0c8a7e1e584d7ffd113f1043887f91ce67
RMS <rms@RMSs-MBP.lan>
2024-03-11 21:27:41 -0300

    Adcionado database e testes

--- fast_zero/schema.py
+++ fast_zero/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class Message(BaseModel):
@@ -13,9 +13,7 @@ class UserPublic(BaseModel):
     id: int
     username: str
     email: EmailStr
-
-class UserDB(UserSchema):
-    id: int
+    model_config = ConfigDict(from_attributes=True)
 
 class UserList(BaseModel):
     users: list[UserPublic]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schema import UserPublic
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
@@ -10,7 +13,7 @@ def test_hello_route_deve_retornar_200_e_HTML(client):
     assert response.status_code == 200
     assert response.text == '<h1>Olá mundo</h1>'
 
-def test_create_user(client):
+def test_create_user(client, user):
     response = client.post(
         '/users',
         json = {
@@ -23,39 +26,58 @@ def test_create_user(client):
     assert response.json() == {
         'username' : 'john',
         'email' : 'example@example.com',
-        'id' : 1,
+        'id' : 2,
     }
 
-def test_list_users(client):
-    response = client.get('/users/')
+def test_create_duplicated_user(client, user):
+    response1 = client.post(
+        '/users',
+        json = {
+            'username' : 'john',
+            'email' : 'example@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response1.status_code == 201
 
+    response = client.post(
+        '/users',
+        json = {
+            'username' : 'john',
+            'email' : 'example@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Username already registered'}
+
+def test_read_users(client):
+    response = client.get('users')
     assert response.status_code == 200
-    assert response.json() == {
-        'users' : [
-            {
-                'username' : 'john',
-                'email' : 'example@example.com',
-                'id' : 1,
-            }
-        ]
-    }
+    assert response.json() == {'users' : []}
 
-def test_update_user(client):
-    response = client.put('/users/1',
-    json = {
-        'username' : 'Bob',
-        'email' : 'bob@example.com',
-        'password' : 'SecretKey'
-    })
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('users')
+    assert response.json() == {'users': [user_schema]}
 
+def test_update_user(client, user):
+    response = client.put(
+        '/users/1',
+        json = {
+            'username' : 'Bob',
+            'email' : 'bob@example.com',
+            'password' : 'SecretKey'
+        },
+    )
     assert response.status_code == 200
     assert response.json() == {
         'username' : 'Bob',
         'email' : 'bob@example.com',
         'id' : 1
-    }
+}
 
-def test_update_user_not_fount(client):
+def test_update_user_not_fount(client, user):
     response = client.put('/users/2',
         json = {
             'username' : 'John',
@@ -66,13 +88,13 @@ def test_update_user_not_fount(client):
     assert response.status_code == 404
     assert response.json() == {'detail' : 'User not found'}
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == 200
     assert response.json() == {'message' : 'User deleted'}
 
-def test_delete_user_not_found(client):
+def test_delete_user_not_found(client, user):
     response = client.delete('/users/2')
 
     assert response.status_code == 404
